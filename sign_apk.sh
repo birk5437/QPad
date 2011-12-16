a5f253c565102209c8ef6c16aa80f079941f4aa5
Burke Ramsey <birk5437@gmail.com>
2011-12-15 19:52:52 -0500

    removed internet permissions requirement

--- sign_apk.sh
+++ sign_apk.sh
@@ -1,2 +1,2 @@
 cd dist
-jarsigner -verbose -keystore /Users/burke/Desktop/android_key/my-release-key.keystore QPad.apk BRamsey
+jarsigner -verbose -keystore /Users/burke/NetBeansProjects/android_release_key/my-release-key.keystore QPad.apk BRamsey
