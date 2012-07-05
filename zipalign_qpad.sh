ab02330791ba3a2aa9fb04ad67239971689f76c6
Burke Ramsey <birk5437@gmail.com>
2012-07-04 20:51:40 -0400

    ready for market

--- zipalign_qpad.sh
+++ zipalign_qpad.sh
@@ -1,5 +1,6 @@
 cd dist
+dir=`pwd`
 mv QPad.apk QPad_unaligned.apk
 cd /Applications/android-sdk-mac_86/tools/
-./zipalign -f -v 4 /Users/burke/NetBeansProjects/QPad/dist/QPad_unaligned.apk /Users/burke/NetBeansProjects/QPad/dist/QPad.apk
-rm /Users/burke/NetBeansProjects/QPad/dist/QPad_unaligned.apk
+./zipalign -f -v 4 $dir/QPad_unaligned.apk $dir/QPad.apk
+rm $dir/QPad_unaligned.apk
